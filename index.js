4c10a0f8f0422ae7fca41ff69090a8873153f01c
benice195 <szczyrbaw@gmail.com>
2017-12-09 17:12:15 +0100

    Serwer poprawiony

--- index.js
+++ index.js
@@ -5,17 +5,36 @@ var server = http.createServer();
 server.on('request', function (request, response) {
     response.setHeader("Content-Type", "text/html; charset=utf-8");
     if (request.method === 'GET' && request.url === '/url') {
-        fs.readFile('./index.html', 'utf-8', function(err, data) {
-            response.write(data);
-            response.end();
-        });            
+        fs.readFile('./index.html', 'utf-8', function(err, data) {            
+            response.write('<head>' +
+              '<meta charset="utf-8">' +
+                '<title>Mój serwer</title>' +
+                '<link rel="stylesheet" href="">' +
+              '</head>' +
+              '<body>' +
+                '<article>' +
+                  '<h1>'+ data +'</h1>' +
+                '</article>' +
+              '</body>' +
+              '<footer>' +
+                '<p></p>' +
+              '</footer>'
+            )
+           response.end();
+        });
+          
     } else {
-            fs.readFile('./cat.jpg', function (err, data) {
-              response.statusCode = 404;
-              response.write('<img src="https://i.ytimg.com/vi/BjChPYhfucg/maxresdefault.jpg">');
-              response.end();
-            })
-            
+        fs.readFile('404.jpg', function (err, data) {
+              response.setHeader("Content-Type", "image/jpeg");
+              /*response.write("<div>");
+              
+              response.write(data);
+              
+              response.write("</div>");
+              response.statusCode = 404;*/
+              
+              response.end(data);
+            })    
     }
 });
 
